f801bb6999594610a2c6c3ab7bbd66fcb9eb77d4
Eloi Simard-Quesnel <eloi.qs@outlook.com>
2018-12-07 21:21:49 -0500

    Add styled-components

--- packages/memes-client/src/index.tsx
+++ packages/memes-client/src/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import { createGlobalStyle } from 'styled-components'
 import * as serviceWorker from './serviceWorker'
+
+const GlobalStyle = createGlobalStyle`
+    body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    }
+
+    code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+`
+
+export const App = () => (
+  <>
+    <GlobalStyle />
+    App
+  </>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
