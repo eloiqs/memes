1c137403fdb1cd52d89ca7648ecae605fb32fd63
Eloi Simard-Quesnel <eloi.qs@outlook.com>
2018-12-09 23:51:21 -0500

    Code review: use qs instead of home made algo

--- packages/memes-graph/src/resolvers/memes-resolver.ts
+++ packages/memes-graph/src/resolvers/memes-resolver.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
-import qs from 'querystring'
+import qs from 'qs'
 import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import CaptionMemeInput from '../types/caption-meme-input'
 import CaptionMemeResponse from '../types/caption-meme-response'
 import Meme from '../types/meme'
-import TextBox from '../types/text-box'
 import IContext from './context'
 
 @Resolver()
@@ -18,44 +17,24 @@ export default class MemesResolver {
   @Mutation(returns => CaptionMemeResponse)
   public async captionMeme(
     @Arg('data')
-    { templateId, font, maxFontSize, boxes: boxesArray }: CaptionMemeInput,
+    { templateId, font, maxFontSize, boxes }: CaptionMemeInput,
     @Ctx() ctx: IContext
   ): Promise<CaptionMemeResponse> {
-    const boxes = boxesArray.reduce(
-      (bxs, box, boxIndex) => {
-        type TextBoxKey = keyof TextBox
-
-        const boxProps = (Object.keys(box) as Array<keyof TextBox>).reduce(
-          (props, prop) => ({
-            ...props,
-            [`boxes[${boxIndex}][${prop}]`]: box[prop]
-          }),
-          {} as { [key in TextBoxKey]: TextBox[key] }
-        )
-
-        return {
-          ...bxs,
-          ...boxProps
-        }
-      },
-      {} as { [key: string]: TextBox[keyof TextBox] }
-    )
-
-    const data = qs.stringify({
-      ...boxes,
-      font,
-      max_font_size: maxFontSize,
-      password: ctx.password,
-      template_id: templateId,
-      username: ctx.username
-    })
-
     const response = (await axios.post<
       | { success: true; data: { url: string; page_url: string } }
       | { success: false; error_message: string }
-    >('https://api.imgflip.com/caption_image', data, {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })).data
+    >(
+      'https://api.imgflip.com/caption_image',
+      qs.stringify({
+        boxes,
+        font,
+        max_font_size: maxFontSize,
+        password: ctx.password,
+        template_id: templateId,
+        username: ctx.username
+      })
+    )).data
+
     if (response.success) {
       return new CaptionMemeResponse({
         pageUrl: response.data.page_url,
