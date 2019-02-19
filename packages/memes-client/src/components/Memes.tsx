62f0f9b08d5f74f2c1b68c4ba695fe55a9f6ab55
Eloi Simard-Quesnel <eloi.qs@outlook.com>
2019-02-18 20:26:39 -0500

    Fix meme implementation in memes list

--- packages/memes-client/src/components/Memes.tsx
+++ packages/memes-client/src/components/Memes.tsx
@@ -1,18 +1,17 @@
 import { navigate, RouteComponentProps } from '@reach/router'
 import React from 'react'
 import MemesQuery from '../queries/MemesQuery'
-import { Meme } from './Meme'
+import Meme from './Meme'
 
 const Memes: React.FunctionComponent<RouteComponentProps> = () => {
   return (
     <MemesQuery>
       {({ memes }) =>
         memes.map(meme => (
-          <Meme
-            key={meme.id}
-            meme={meme}
-            onMemeClick={() => navigate(`memes/${meme.id}`)}
-          />
+          <div key={meme.id} onClick={() => navigate(`memes/${meme.id}`)}>
+            <h3>{meme.name}</h3>
+            <Meme meme={meme} />
+          </div>
         ))
       }
     </MemesQuery>
